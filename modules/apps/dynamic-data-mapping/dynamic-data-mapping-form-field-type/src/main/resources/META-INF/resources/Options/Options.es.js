5b1dab0f32860169f9603a41daedbf938168311b
Carolina <carolina.barbosa@liferay.com>
2020-06-18 08:08:10 -0300

    LPS-114467 Allow interactions when translating a form for Options field type

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Options/Options.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Options/Options.es.js
@@ -77,9 +77,7 @@ const refreshFields = (defaultLanguageId, editingLanguageId, options) =>
 			),
 			id: random(),
 		})),
-		defaultLanguageId === editingLanguageId
-			? {...defaultOption, generateKeyword: true, id: random()}
-			: false,
+		{...defaultOption, generateKeyword: true, id: random()},
 	].filter((field) => field && Object.keys(field).length > 0);
 
 const Options = ({
@@ -161,7 +159,7 @@ const Options = ({
 	};
 
 	const synchroniseValue = (fields, languageId) => {
-		if (defaultLanguageId === languageId) {
+		if (editingLanguageId === languageId) {
 			return [...fields];
 		}
 
@@ -205,13 +203,11 @@ const Options = ({
 	const add = (fields, index, property, value) => {
 		fields[index][property] = value;
 
-		if (defaultLanguageId === editingLanguageId) {
-			fields.push({
-				...defaultOption,
-				generateKeyword: true,
-				id: random(),
-			});
-		}
+		fields.push({
+			...defaultOption,
+			generateKeyword: true,
+			id: random(),
+		});
 
 		return [fields, index, property, value];
 	};
@@ -322,9 +318,7 @@ const Main = ({
 					<KeyValue
 						generateKeyword={option.generateKeyword}
 						keyword={option.value}
-						keywordReadOnly={
-							defaultLanguageId !== editingLanguageId
-						}
+						keywordReadOnly={false}
 						name={`option${index}`}
 						onBlur={handleBlur}
 						onChange={(event) =>
