39677713ee330c73bab8c1774f6f9401bec410d8
Carolina <carolina.barbosa@liferay.com>
2020-11-11 17:13:59 -0300

    LPS-123363 Create new setting on Upload field to enable/disable the file upload by Guest user

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributor.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Html;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
@@ -87,8 +88,11 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributor
 		}
 
 		parameters.put(
+			"allowGuestUsers",
+			GetterUtil.getBoolean(
+				ddmFormFieldRenderingContext.getProperty("allowGuestUsers")));
+		parameters.put(
 			"groupId", ddmFormFieldRenderingContext.getProperty("groupId"));
-
 		parameters.put(
 			"itemSelectorAuthToken",
 			getItemSelectorAuthToken(httpServletRequest));
