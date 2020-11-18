59b0fe11cab8558a0f31b6e3c8408e4eb0e3a27b
Carolina <carolina.barbosa@liferay.com>
2020-11-18 17:00:45 -0300

    LPS-123483 Increment test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributorTest.java
@@ -184,6 +184,9 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 					"/upload_file_entry"));
 		Assert.assertThat(
 			uploadURL,
+			CoreMatchers.containsString("param_folderId=" + _FOLDER_ID));
+		Assert.assertThat(
+			uploadURL,
 			CoreMatchers.containsString(
 				"param_formInstanceId=" + _FORM_INSTANCE_ID));
 		Assert.assertThat(
