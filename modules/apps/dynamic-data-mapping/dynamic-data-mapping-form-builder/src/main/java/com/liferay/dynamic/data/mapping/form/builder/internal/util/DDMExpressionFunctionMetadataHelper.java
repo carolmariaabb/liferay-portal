c46eb67bbf12e862f69053f71670b96a010a9121
Carolina <carolina.barbosa@liferay.com>
2021-05-10 17:38:16 -0300

    LPS-131539 Populate ddmExpressionFunctionsMetadata map with custom functions

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/java/com/liferay/dynamic/data/mapping/form/builder/internal/util/DDMExpressionFunctionMetadataHelper.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/java/com/liferay/dynamic/data/mapping/form/builder/internal/util/DDMExpressionFunctionMetadataHelper.java
@@ -14,17 +14,25 @@
 
 package com.liferay.dynamic.data.mapping.form.builder.internal.util;
 
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunctionTracker;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.AggregateResourceBundle;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
+import java.lang.reflect.Method;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -83,6 +91,50 @@ public class DDMExpressionFunctionMetadataHelper {
 
 	}
 
+	protected void addCustomDDMExpressionFunctionsMetadata(
+		Map<String, List<DDMExpressionFunctionMetadata>>
+			ddmExpressionFunctionsMetadata) {
+
+		Map<String, DDMExpressionFunction> customDDMExpressionFunctions =
+			_ddmExpressionFunctionTracker.getCustomDDMExpressionFunctions();
+
+		for (Map.Entry<String, DDMExpressionFunction> entry :
+				customDDMExpressionFunctions.entrySet()) {
+
+			DDMExpressionFunction ddmExpressionFunction = entry.getValue();
+
+			Class<?> clazz = ddmExpressionFunction.getClass();
+
+			Stream<Method> stream = Arrays.stream(clazz.getMethods());
+
+			Optional<Method> optional = stream.filter(
+				method ->
+					Objects.equals(method.getName(), "apply") &&
+					Objects.equals(method.getReturnType(), Boolean.class)
+			).findFirst();
+
+			if (!optional.isPresent()) {
+				continue;
+			}
+
+			Method method = optional.get();
+
+			addDDMExpressionFunctionMetadata(
+				ddmExpressionFunctionsMetadata,
+				new DDMExpressionFunctionMetadata(
+					entry.getKey(), entry.getKey(), _TYPE_BOOLEAN,
+					_getParameterTypes(
+						method.getParameterCount(), _TYPE_NUMBER)));
+
+			addDDMExpressionFunctionMetadata(
+				ddmExpressionFunctionsMetadata,
+				new DDMExpressionFunctionMetadata(
+					entry.getKey(), entry.getKey(), _TYPE_BOOLEAN,
+					_getParameterTypes(
+						method.getParameterCount(), _TYPE_TEXT)));
+		}
+	}
+
 	protected void addDDMExpressionFunctionMetadata(
 		Map<String, List<DDMExpressionFunctionMetadata>>
 			ddmExpressionFunctionsMetadata,
@@ -118,6 +170,8 @@ public class DDMExpressionFunctionMetadataHelper {
 		Map<String, List<DDMExpressionFunctionMetadata>>
 			ddmExpressionFunctionsMetadata,
 		ResourceBundle resourceBundle) {
+
+		addCustomDDMExpressionFunctionsMetadata(ddmExpressionFunctionsMetadata);
 
 		addDDMExpressionFunctionMetadata(
 			ddmExpressionFunctionsMetadata,
@@ -218,6 +272,16 @@ public class DDMExpressionFunctionMetadataHelper {
 				new String[] {_TYPE_TEXT}));
 	}
 
+	private String[] _getParameterTypes(
+		int parameterCount, String parameterType) {
+
+		String[] parameterTypes = new String[parameterCount];
+
+		Arrays.fill(parameterTypes, parameterType);
+
+		return parameterTypes;
+	}
+
 	private static final String _TYPE_BOOLEAN = "boolean";
 
 	private static final String _TYPE_LIST = "list";
@@ -227,6 +291,9 @@ public class DDMExpressionFunctionMetadataHelper {
 	private static final String _TYPE_TEXT = "text";
 
 	private static final String _TYPE_USER = "user";
+
+	@Reference
+	private DDMExpressionFunctionTracker _ddmExpressionFunctionTracker;
 
 	@Reference
 	private Portal _portal;
