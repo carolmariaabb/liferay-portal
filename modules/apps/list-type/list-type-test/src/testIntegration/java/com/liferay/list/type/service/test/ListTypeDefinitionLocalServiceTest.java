48d6d4bbdeb30986a5806f199b06545e3d02a7d5
Carolina Barbosa <carolina.barbosa@liferay.com>
2021-09-14 15:42:01 -0300

    LPS-138737 Increment test

--- modules/apps/list-type/list-type-test/src/testIntegration/java/com/liferay/list/type/service/test/ListTypeDefinitionLocalServiceTest.java
+++ modules/apps/list-type/list-type-test/src/testIntegration/java/com/liferay/list/type/service/test/ListTypeDefinitionLocalServiceTest.java
@@ -15,15 +15,23 @@
 package com.liferay.list.type.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.list.type.exception.RequiredListTypeDefinitionException;
 import com.liferay.list.type.model.ListTypeDefinition;
 import com.liferay.list.type.model.ListTypeEntry;
 import com.liferay.list.type.service.ListTypeDefinitionLocalServiceUtil;
 import com.liferay.list.type.service.ListTypeEntryLocalServiceUtil;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.util.LocalizedMapUtil;
+import com.liferay.object.util.ObjectFieldUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.Collections;
@@ -77,6 +85,35 @@ public class ListTypeDefinitionLocalServiceTest {
 
 		Assert.assertNotNull(listTypeEntry);
 
+		ObjectField objectField = ObjectFieldUtil.createObjectField(
+			StringUtil.randomId(), "String");
+
+		objectField.setListTypeDefinitionId(
+			listTypeDefinition.getListTypeDefinitionId());
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap("Test"), "Test", null, null,
+				LocalizedMapUtil.getLocalizedMap("Tests"),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				Collections.singletonList(objectField));
+
+		try {
+			ListTypeDefinitionLocalServiceUtil.deleteListTypeDefinition(
+				listTypeDefinition.getListTypeDefinitionId());
+
+			Assert.fail();
+		}
+		catch (RequiredListTypeDefinitionException
+					requiredListTypeDefinitionException) {
+
+			Assert.assertNotNull(requiredListTypeDefinitionException);
+		}
+
+		_objectDefinitionLocalService.deleteObjectDefinition(
+			objectDefinition.getObjectDefinitionId());
+
 		ListTypeDefinitionLocalServiceUtil.deleteListTypeDefinition(
 			listTypeDefinition.getListTypeDefinitionId());
 
@@ -87,5 +124,8 @@ public class ListTypeDefinitionLocalServiceTest {
 			ListTypeEntryLocalServiceUtil.fetchListTypeEntry(
 				listTypeEntry.getListTypeEntryId()));
 	}
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 }
