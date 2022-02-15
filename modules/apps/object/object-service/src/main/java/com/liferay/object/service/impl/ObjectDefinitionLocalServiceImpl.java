e2645c4cee9293511150ec822201a56f7686dc16
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-02-15 11:08:43 -0300

    LPS-147113 Update method calls

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -708,7 +708,7 @@ public class ObjectDefinitionLocalServiceImpl
 						objectField.isIndexedAsKeyword(),
 						objectField.getIndexedLanguageId(),
 						objectField.getLabelMap(), objectField.getName(),
-						objectField.isRequired());
+						objectField.isRequired(), Collections.emptyList());
 				}
 			}
 		}
