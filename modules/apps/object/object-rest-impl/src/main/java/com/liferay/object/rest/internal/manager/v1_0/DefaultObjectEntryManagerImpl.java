646974a656d93429cfd8b6e21f6efd78b2a77ec5
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-07-04 11:18:32 -0300

    LPD-40798 When relating entries between different scopes, use the correct groupId, particularly if the related object definition is scoped by site

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -1192,6 +1192,13 @@ public class DefaultObjectEntryManagerImpl
 							serviceBuilderObjectEntry.getPrimaryKey());
 					}
 
+					long relatedGroupId =
+						ObjectRelationshipUtil.getRelatedGroupId(
+							groupLocalService, objectDefinition,
+							objectScopeProviderRegistry,
+							relatedObjectDefinition, scopeKey,
+							nestedObjectEntry.getScopeKey());
+
 					try {
 						nestedObjectEntry =
 							objectEntryManager.updateObjectEntry(
@@ -1199,7 +1206,7 @@ public class DefaultObjectEntryManagerImpl
 								dtoConverterContext,
 								nestedObjectEntry.getExternalReferenceCode(),
 								relatedObjectDefinition, nestedObjectEntry,
-								scopeKey);
+								String.valueOf(relatedGroupId));
 					}
 					catch (ObjectEntryValuesException.Required
 								objectEntryValuesException) {
@@ -1208,23 +1215,14 @@ public class DefaultObjectEntryManagerImpl
 							throw objectEntryValuesException;
 						}
 
-						long groupId = 0;
-
-						if ((serviceBuilderObjectEntry.getGroupId() > 0) &&
-							Objects.equals(
-								relatedObjectDefinition.getScope(),
-								ObjectDefinitionConstants.SCOPE_SITE)) {
-
-							groupId = serviceBuilderObjectEntry.getGroupId();
-						}
-
 						nestedObjectEntry = _toObjectEntry(
 							dtoConverterContext, relatedObjectDefinition,
 							objectEntryLocalService.
 								getOrAddIncompleteObjectEntry(
 									nestedObjectEntry.
 										getExternalReferenceCode(),
-									groupId, dtoConverterContext.getUserId(),
+									relatedGroupId,
+									dtoConverterContext.getUserId(),
 									relatedObjectDefinition.
 										getObjectDefinitionId()));
 					}
--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/vulcan/extension/v1_0/ObjectRelationshipExtensionProvider.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/vulcan/extension/v1_0/ObjectRelationshipExtensionProvider.java
@@ -244,7 +244,7 @@ public class ObjectRelationshipExtensionProvider
 						objectDefinition, primaryKey, null, userId),
 					nestedObjectEntry.getExternalReferenceCode(),
 					relatedObjectDefinition, nestedObjectEntry,
-					relatedObjectDefinition.getScope());
+					nestedObjectEntry.getScopeKey());
 
 				_relateNestedObjectEntry(
 					objectDefinition, objectRelationship, primaryKey,
