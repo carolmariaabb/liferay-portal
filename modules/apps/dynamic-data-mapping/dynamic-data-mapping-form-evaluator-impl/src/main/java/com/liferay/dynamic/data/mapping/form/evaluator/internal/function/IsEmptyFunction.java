1dcf842502f77a61e790e7cf52c11a8dfa31fe40
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-09-26 10:16:38 -0300

    LPD-66414 Make it possible to use the isEmpty function with the assignee object field

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/IsEmptyFunction.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/main/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/function/IsEmptyFunction.java
@@ -8,7 +8,10 @@ package com.liferay.dynamic.data.mapping.form.evaluator.internal.function;
 import com.liferay.dynamic.data.mapping.expression.DDMExpressionFunction;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Map;
 
 /**
  * @author Leonardo Barros
@@ -32,6 +35,22 @@ public class IsEmptyFunction
 					return false;
 				}
 			}
+		}
+
+		if (parameter instanceof Map) {
+			Map<?, ?> map = (Map<?, ?>)parameter;
+
+			if (MapUtil.isEmpty(map)) {
+				return true;
+			}
+
+			for (Object object : map.values()) {
+				if (Validator.isNotNull(object)) {
+					return false;
+				}
+			}
+
+			return true;
 		}
 
 		if (parameter instanceof JSONArray) {
