292e35650e342182a7ef77cd3cf37448db07424d
Caio Farias <caio.farias@liferay.com>
2025-10-14 11:22:50 -0300

    LPD-59867 add object definition validation exception and object definition validation exception mapper

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectDefinitionValidationException.java
@@ -0,0 +1,68 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+
+/**
+ * @author Caio Farias
+ */
+public class ObjectDefinitionValidationException extends PortalException {
+
+	public List<ValidationError> getValidationErrors() {
+		return _validationErrors;
+	}
+
+	public void setValidationErrors(List<ValidationError> validationErrors) {
+		_validationErrors = validationErrors;
+	}
+
+	public static class ValidationError {
+
+		public ValidationError(
+			String className, String errorMessage, String exceptionClassName,
+			String property, Object value) {
+
+			_className = className;
+			_errorMessage = errorMessage;
+			_exceptionClassName = exceptionClassName;
+			_property = property;
+			_value = value;
+		}
+
+		public String getClassName() {
+			return _className;
+		}
+
+		public String getErrorMessage() {
+			return _errorMessage;
+		}
+
+		public String getExceptionClassName() {
+			return _exceptionClassName;
+		}
+
+		public String getProperty() {
+			return _property;
+		}
+
+		public Object getValue() {
+			return _value;
+		}
+
+		private final String _className;
+		private final String _errorMessage;
+		private final String _exceptionClassName;
+		private final String _property;
+		private final Object _value;
+
+	}
+
+	private List<ValidationError> _validationErrors;
+
+}
