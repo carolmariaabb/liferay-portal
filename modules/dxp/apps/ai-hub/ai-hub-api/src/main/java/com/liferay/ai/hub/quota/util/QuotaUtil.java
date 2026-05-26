226c17a083ef992b291767963bf6b8aecbbd33d6
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-05-26 13:59:49 -0300

    LPD-92138 Implement monthly quota refresh endpoint

--- modules/dxp/apps/ai-hub/ai-hub-api/src/main/java/com/liferay/ai/hub/quota/util/QuotaUtil.java
+++ modules/dxp/apps/ai-hub/ai-hub-api/src/main/java/com/liferay/ai/hub/quota/util/QuotaUtil.java
@@ -11,7 +11,10 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
 import com.liferay.object.service.ObjectEntryLocalServiceUtil;
+import com.liferay.portal.kernel.audit.AuditMessage;
+import com.liferay.portal.kernel.audit.AuditRouterUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.lock.Lock;
 import com.liferay.portal.kernel.lock.LockManagerUtil;
 import com.liferay.portal.kernel.model.User;
@@ -21,11 +24,18 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+import com.liferay.portal.security.audit.event.generators.util.AuditMessageBuilder;
 
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.Serializable;
 
+import java.time.Instant;
+import java.time.YearMonth;
+import java.time.ZoneId;
+
+import java.util.Date;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
@@ -60,6 +70,79 @@ public class QuotaUtil {
 		}
 	}
 
+	public static void refreshUsage(
+			long companyId, String externalReferenceCode, long userId)
+		throws PortalException {
+
+		ObjectDefinition objectDefinition =
+			ObjectDefinitionLocalServiceUtil.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_AI_HUB_QUOTA", companyId);
+
+		if (objectDefinition == null) {
+			return;
+		}
+
+		ObjectEntry objectEntry = ObjectEntryLocalServiceUtil.fetchObjectEntry(
+			externalReferenceCode, 0, objectDefinition.getObjectDefinitionId());
+
+		if (objectEntry == null) {
+			return;
+		}
+
+		Map<String, Serializable> values = objectEntry.getValues();
+
+		long lrtUsage = MapUtil.getLong(values, "lrtUsage");
+		long usage = MapUtil.getLong(values, "usage");
+
+		if ((lrtUsage == 0) && (usage == 0)) {
+			return;
+		}
+
+		Date currentDate = new Date();
+		Date lastRefreshDate = (Date)values.get("lastRefreshDate");
+
+		if ((lastRefreshDate != null) &&
+			Objects.equals(
+				YearMonth.from(_toYearMonth(currentDate)),
+				YearMonth.from(_toYearMonth(lastRefreshDate)))) {
+
+			return;
+		}
+
+		try (Closeable closeable = _lock(objectEntry.getObjectEntryId())) {
+			objectEntry = ObjectEntryLocalServiceUtil.getObjectEntry(
+				objectEntry.getObjectEntryId());
+
+			ObjectEntryLocalServiceUtil.partialUpdateObjectEntry(
+				userId, objectEntry.getObjectEntryId(), 0,
+				HashMapBuilder.<String, Serializable>put(
+					"lastRefreshDate", currentDate.toString()
+				).put(
+					"lrtUsage", 0L
+				).put(
+					"usage", 0L
+				).build(),
+				_getServiceContext(companyId, userId));
+
+			AuditMessage auditMessage = AuditMessageBuilder.buildAuditMessage(
+				objectEntry, "REFRESH", null);
+
+			JSONObject additionalInfoJSONObject =
+				auditMessage.getAdditionalInfo();
+
+			additionalInfoJSONObject.put(
+				"accountEntryId",
+				MapUtil.getLong(
+					values, "r_accountToAIHubQuotas_accountEntryId"));
+
+			AuditRouterUtil.route(auditMessage);
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+	}
+
 	public static void updateUsage(
 			long companyId, long milliLRTCount, long tokensCount, long userId)
 		throws PortalException {
@@ -74,13 +157,18 @@ public class QuotaUtil {
 			objectEntry = ObjectEntryLocalServiceUtil.getObjectEntry(
 				objectEntry.getObjectEntryId());
 
-			_partialUpdateObjectEntry(
-				companyId,
-				MapUtil.getLong(objectEntry.getValues(), "lrtUsage") +
-					milliLRTCount,
-				objectEntry,
-				MapUtil.getLong(objectEntry.getValues(), "usage") + tokensCount,
-				userId);
+			ObjectEntryLocalServiceUtil.partialUpdateObjectEntry(
+				userId, objectEntry.getObjectEntryId(), 0,
+				HashMapBuilder.<String, Serializable>put(
+					"lrtUsage",
+					MapUtil.getLong(objectEntry.getValues(), "lrtUsage") +
+						milliLRTCount
+				).put(
+					"usage",
+					MapUtil.getLong(objectEntry.getValues(), "usage") +
+						tokensCount
+				).build(),
+				_getServiceContext(companyId, userId));
 		}
 		catch (IOException ioException) {
 			throw new RuntimeException(ioException);
@@ -121,6 +209,17 @@ public class QuotaUtil {
 			externalReferenceCode, 0, objectDefinition.getObjectDefinitionId());
 	}
 
+	private static ServiceContext _getServiceContext(
+		long companyId, long userId) {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setCompanyId(companyId);
+		serviceContext.setUserId(userId);
+
+		return serviceContext;
+	}
+
 	private static Closeable _lock(long objectEntryId) throws PortalException {
 		String updatedOwner = PortalUUIDUtil.generate();
 
@@ -156,24 +255,10 @@ public class QuotaUtil {
 			updatedOwner);
 	}
 
-	private static void _partialUpdateObjectEntry(
-			long companyId, long lrtUsage, ObjectEntry objectEntry, long usage,
-			long userId)
-		throws PortalException {
+	private static YearMonth _toYearMonth(Date date) {
+		Instant instant = Instant.ofEpochMilli(date.getTime());
 
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setCompanyId(companyId);
-		serviceContext.setUserId(userId);
-
-		ObjectEntryLocalServiceUtil.partialUpdateObjectEntry(
-			userId, objectEntry.getObjectEntryId(), 0,
-			HashMapBuilder.<String, Serializable>put(
-				"lrtUsage", lrtUsage
-			).put(
-				"usage", usage
-			).build(),
-			serviceContext);
+		return YearMonth.from(instant.atZone(ZoneId.systemDefault()));
 	}
 
 }
--- modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/resource/v1_0/QuotaResourceImpl.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/resource/v1_0/QuotaResourceImpl.java
@@ -5,17 +5,63 @@
 
 package com.liferay.ai.hub.rest.internal.resource.v1_0;
 
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryService;
+import com.liferay.ai.hub.quota.util.QuotaUtil;
 import com.liferay.ai.hub.rest.resource.v1_0.QuotaResource;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+
+import jakarta.ws.rs.NotAuthorizedException;
+import jakarta.ws.rs.core.Response;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
- * @author Feliphe Marinho
+ * @author Carolina Barbosa
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/quota.properties",
 	scope = ServiceScope.PROTOTYPE, service = QuotaResource.class
 )
 public class QuotaResourceImpl extends BaseQuotaResourceImpl {
+
+	@Override
+	public void putAccountAccountEntryQuotaRefreshMonthly(Long accountEntryId)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				contextCompany.getCompanyId(), "LPD-62272")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (!permissionChecker.isCompanyAdmin() &&
+			!permissionChecker.isOmniadmin()) {
+
+			throw new NotAuthorizedException(Response.Status.UNAUTHORIZED);
+		}
+
+		AccountEntry accountEntry = _accountEntryService.getAccountEntry(
+			accountEntryId);
+
+		QuotaUtil.refreshUsage(
+			contextCompany.getCompanyId(),
+			"guest-quota-" + accountEntry.getAccountEntryId(),
+			contextUser.getUserId());
+		QuotaUtil.refreshUsage(
+			contextCompany.getCompanyId(),
+			"quota-" + accountEntry.getAccountEntryId(),
+			contextUser.getUserId());
+	}
+
+	@Reference
+	private AccountEntryService _accountEntryService;
+
 }
