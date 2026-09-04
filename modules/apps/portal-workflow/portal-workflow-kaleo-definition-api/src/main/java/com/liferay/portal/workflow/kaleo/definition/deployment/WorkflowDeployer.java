ada86b424a05d512d5e31aa90626c830f49bbe5e
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-09-04 12:04:35 -0300

    LPD-104035 Add new interface methods

--- modules/apps/portal-workflow/portal-workflow-kaleo-definition-api/src/main/java/com/liferay/portal/workflow/kaleo/definition/deployment/WorkflowDeployer.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-definition-api/src/main/java/com/liferay/portal/workflow/kaleo/definition/deployment/WorkflowDeployer.java
@@ -15,6 +15,15 @@ import com.liferay.portal.workflow.kaleo.definition.Definition;
  */
 public interface WorkflowDeployer {
 
+	public default WorkflowDefinition deploy(
+			boolean active, Definition definition, String externalReferenceCode,
+			String name, String scope, ServiceContext serviceContext,
+			boolean system, String title, int version)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
 	public WorkflowDefinition deploy(
 			String externalReferenceCode, String title, String name,
 			String scope, boolean system, Definition definition,
