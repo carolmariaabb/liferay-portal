067c5b2884bc5709be8a28ca71d1aa06c9b02959
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-09-09 11:24:10 -0300

    LPD-104035 Route workflow definition UPSERT through the by-ERC write path

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
@@ -45,9 +45,6 @@ import com.liferay.portal.workflow.manager.WorkflowDefinitionManager;
 
 import jakarta.ws.rs.core.MultivaluedMap;
 
-import java.io.Serializable;
-
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -69,35 +66,6 @@ public class WorkflowDefinitionResourceImpl
 	extends BaseWorkflowDefinitionResourceImpl {
 
 	@Override
-	public void create(
-			Collection<WorkflowDefinition> workflowDefinitions,
-			Map<String, Serializable> parameters)
-		throws Exception {
-
-		String createStrategy = (String)parameters.getOrDefault(
-			"createStrategy", "INSERT");
-
-		if (StringUtil.equalsIgnoreCase(createStrategy, "UPSERT")) {
-			if (contextBatchUnsafeConsumer != null) {
-				contextBatchUnsafeConsumer.accept(
-					workflowDefinitions,
-					workflowDefinition -> postWorkflowDefinitionDeploy(
-						workflowDefinition));
-			}
-			else {
-				for (WorkflowDefinition workflowDefinition :
-						workflowDefinitions) {
-
-					postWorkflowDefinitionDeploy(workflowDefinition);
-				}
-			}
-		}
-		else {
-			super.create(workflowDefinitions, parameters);
-		}
-	}
-
-	@Override
 	public void deleteWorkflowDefinition(Long workflowDefinitionId)
 		throws Exception {
 
@@ -110,6 +78,20 @@ public class WorkflowDefinitionResourceImpl
 
 		deleteWorkflowDefinitionUndeploy(
 			workflowDefinition.getName(), workflowDefinition.getVersion());
+	}
+
+	@Override
+	public void deleteWorkflowDefinitionByExternalReferenceCode(
+			String externalReferenceCode)
+		throws Exception {
+
+		com.liferay.portal.kernel.workflow.WorkflowDefinition
+			serviceBuilderWorkflowDefinition =
+				_workflowDefinitionManager.getWorkflowDefinition(
+					contextCompany.getCompanyId(), externalReferenceCode);
+
+		deleteWorkflowDefinition(
+			serviceBuilderWorkflowDefinition.getWorkflowDefinitionId());
 	}
 
 	@Override
@@ -272,6 +254,17 @@ public class WorkflowDefinitionResourceImpl
 
 		_workflowDefinitionManager.getLatestWorkflowDefinition(
 			contextCompany.getCompanyId(), workflowDefinition.getName());
+
+		return postWorkflowDefinitionDeploy(workflowDefinition);
+	}
+
+	@Override
+	public WorkflowDefinition putWorkflowDefinitionByExternalReferenceCode(
+			String externalReferenceCode, WorkflowDefinition workflowDefinition)
+		throws Exception {
+
+		workflowDefinition.setExternalReferenceCode(
+			() -> externalReferenceCode);
 
 		return postWorkflowDefinitionDeploy(workflowDefinition);
 	}
